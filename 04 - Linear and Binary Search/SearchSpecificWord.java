e88ad69e433886b3d79fcfdb8cf79608f56fc416
Anmol Dhiman <73706789+AnmolDh@users.noreply.github.com>
2025-03-19 13:23:13 +0530

    feat: implemented Search in Sentance

---
+++ 04 - Linear and Binary Search/SearchSpecificWord.java
@@ -0,0 +1,24 @@
+public class SearchSpecificWord{
+  public static String findWord(String[] sentences, String word){
+    for (String sentence : sentences) {
+            if (sentence.contains(word)) {
+                return sentence;
+            }
+        }
+        return "Not Found";
+  }
+
+  public static void main(String[] args){
+    String[] sentences = {
+            "This is the first sentence.",
+            "Another example of a sentence.",
+            "Here is the word we are looking for: magic.",
+            "Yet another sentence with no match."
+        };
+        
+        String searchWord = "magic";
+
+        String result = findWord(sentences, searchWord);
+        System.out.println(result);
+  }
+}

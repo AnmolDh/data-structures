f743a031289722e27a08940279e2956379f471ef
Anmol Dhiman <73706789+AnmolDh@users.noreply.github.com>
2025-03-19 13:18:40 +0530

    feat: implemented Find First Negative

---
+++ 04 - Linear and Binary Search/FirstNegative.java
@@ -0,0 +1,16 @@
+public class FirstNegative{
+
+  public static int FirstNegative(int[] arr){
+    for (int i = 0; i < arr.length; i++){
+      if (arr[i] < 0) return arr[i];
+    }
+
+    return -1;
+  }
+
+  public static void main(String[] args){
+    int[] arr = {29, 324, 525, 661, -33, 313};
+
+    System.out.println(FirstNegative(arr));
+  }
+}

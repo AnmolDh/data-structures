5647b7eabcae92bba03c366e6bc034bf1cff0075
Anmol Dhiman <73706789+AnmolDh@users.noreply.github.com>
2025-03-19 13:11:14 +0530

    feat: implemented FileReader using InputStream

--- 04 - Linear and Binary Search/ByteIntoCharacterStreams.java
+++ 04 - Linear and Binary Search/ByteIntoCharacterStreams.java
@@ -1,13 +1,15 @@
 import java.io.BufferedReader;
-import java.io.FileReader;
+import java.io.FileInputStream;
+import java.io.InputStreamReader;
 import java.io.IOException;
 
-public class FileLineReader {
+public class ByteIntoCharacterStreams {
     public static void main(String[] args) {
         String filePath = "./example.txt";
-        try (BufferedReader br = new BufferedReader(new FileReader(filePath))) {
+        try (BufferedReader reader = new BufferedReader(
+                new InputStreamReader(new FileInputStream(filePath), "UTF-8"))) {
             String line;
-            while ((line = br.readLine()) != null) {
+            while ((line = reader.readLine()) != null) {
                 System.out.println(line);
             }
         } catch (IOException e) {
